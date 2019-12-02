a429280659af63eea35b8a67fc62ff4be61d8424
Charles Gourdin <gourdin.charles@gmail.com>
2019-12-02 22:51:50 +0100

    Mise en place des cartes room sans mise en forme

--- src/pages/RoomList.js
+++ src/pages/RoomList.js
@@ -1,15 +1,18 @@
 import React, { useContext } from 'react'
 import { PrincipalContext } from '../providers/PrincipalContext'
+import './Pages.css'
 
 const RoomList = props => {
     const { rooms } = useContext(PrincipalContext)
     return (
         <>
-            <>
-                <div>
+            <>  <div>
+
+                </div>
+                <div className='roomContainer'>
                     {rooms.map((item, i) => {
                         return (
-                            <div key={i} style={{border: '1px black solid'}}>
+                            <div className="roomCard" key={i}>
                                 <p>{item.name}</p>
                                 <p>{item.address}</p>
                                 <p>{item.zip}</p>
